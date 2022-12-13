99a6a735e0e7358ec05213ae9c453335025960a2
RadoslavSt <boba.radoslav@gmail.com>
2022-12-13 11:37:52 +0100

    finish

---
+++ cypress/e2e/APIexamples2.cy.js
@@ -0,0 +1,59 @@
+/// <reference types = 'cypress'/>
+
+describe('API examples2', ()=>{
+    const userCredentials = {
+        email: "bobaTest@gmail.com",
+        password: "test12345",
+      };
+
+    it("Get all my galleries and changed data to the last one", () => {
+        const changedData = {
+          title: "changed Title with Api",
+          description: "changed description by api",
+          images: [
+            "https://media.istockphoto.com/id/1191223442/photo/lilac-breasted-roller-in-kenya-africa.jpg?b=1&s=170667a&w=0&k=20&c=Nkdvv7lEvTpLa4QZcUQC9krKVxfnfOa89QuNd4FqaNk=",
+          ],
+        };
+    
+        cy.api(
+          "POST",
+          "https://gallery-api.vivifyideas.com/api/auth/login",
+          userCredentials
+        )
+          .its("body")
+          .then((body) => {
+            console.log(body);
+            const token2 = body.access_token;
+            cy.log(token2);
+    
+            cy.request({
+              method: "GET",
+              url: "https://gallery-api.vivifyideas.com/api/my-galleries?page=1&term=",
+              headers: { authorization: "Bearer " + token2 },
+            }).then((respGallerys) => {
+              console.log(respGallerys);
+              cy.log('Trenutni broj mojih galerija je: ' + respGallerys.body.count);
+             // cy.log(respGallerys.body.galleries);
+    
+              cy.log('ID moje najnovije galerije je: ' + respGallerys.body.galleries[0].id);
+              const lastGalleryId = respGallerys.body.galleries[0].id;
+    
+              cy.request({
+                method: "PUT",
+                url:
+                  "https://gallery-api.vivifyideas.com/api/galleries/" +
+                  lastGalleryId,
+                headers: { authorization: "Bearer " + token2 },
+                body: changedData,
+              });
+            });
+          });
+      });
+
+
+
+
+
+
+
+})
---
+++ cypress/e2e/APIexamples3.cy.js
@@ -0,0 +1,50 @@
+/// <reference types = 'cypress'/>
+
+
+
+describe("API examples 3", () => {
+  it("Get my galleries and Delete the last one", () => {
+    const userCredentials = {
+      email: "bobaTest@gmail.com",
+      password: "test12345",
+    };
+
+    cy.api(
+      "POST",
+      "https://gallery-api.vivifyideas.com/api/auth/login",
+      userCredentials
+    )
+      .its("body")
+      .then((body) => {
+        console.log(body);
+        const token3 = body.access_token;
+        cy.log(token3);
+
+        cy.request({
+          method: "GET",
+          url: "https://gallery-api.vivifyideas.com/api/my-galleries?page=1&term=",
+          headers: { authorization: "Bearer " + token3 },
+        }).then((respons)=>{
+            console.log(respons)
+            cy.log('Trenutni broj mojih galerija je:'+ respons.body.count)
+            cy.log(respons.body.galleries[0].id)
+            const lastID = respons.body.galleries[0].id
+
+        
+
+        cy.request({
+            method:'DELETE',
+            url: 'https://gallery-api.vivifyideas.com/api/galleries/'+lastID,
+            headers:{authorization:'Bearer ' + token3}
+
+        }).then((resp)=>{
+            console.log(resp)
+           // expect(resp.method).eq('DELETE')
+        })
+
+    });
+
+
+      });
+  });
+});
